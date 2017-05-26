650f69966bbadfea20e1935e99402dab4206dfb4
Anton Savoskin <sairus2k@gmail.com>
2017-05-26 11:09:03 +0700

    Add deployment task

--- gulp_tasks/misc.js
+++ gulp_tasks/misc.js
@@ -3,11 +3,13 @@ const path = require('path');
 const gulp = require('gulp');
 const del = require('del');
 const filter = require('gulp-filter');
+const ghPages = require('gulp-gh-pages');
 
 const conf = require('../conf/gulp.conf');
 
 gulp.task('clean', clean);
 gulp.task('other', other);
+gulp.task('deploy:gh-pages', deploy);
 
 function clean() {
   return del([conf.paths.dist, conf.paths.tmp]);
@@ -22,4 +24,9 @@ function other() {
   ])
     .pipe(fileFilter)
     .pipe(gulp.dest(conf.paths.dist));
+}
+
+function deploy() {
+  return gulp.src(path.join(conf.paths.dist, '/**/*'))
+    .pipe(ghPages());
 }
